b586234da9d01364a8535342b904581528604cb6
Valentin <valentin7petr@gmail.com>
2023-07-23 16:42:46 +0300

    Initial commit

---
+++ src/test/java/ru/yakaska/tenki/TenkiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakaska.tenki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
