8eff69d937143f177a73c942aec307e27fcb764c
Yakaska <valentin7petr@gmail.com>
2023-08-10 11:03:52 +0300

    Fix: added Repository annotation to repositories

--- src/main/java/ru/yakaska/tenki/repository/LocationRepository.java
+++ src/main/java/ru/yakaska/tenki/repository/LocationRepository.java
@@ -1,8 +1,10 @@
 package ru.yakaska.tenki.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.yakaska.tenki.entity.Location;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
 
--- src/main/java/ru/yakaska/tenki/repository/UserRepository.java
+++ src/main/java/ru/yakaska/tenki/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package ru.yakaska.tenki.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.yakaska.tenki.entity.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
