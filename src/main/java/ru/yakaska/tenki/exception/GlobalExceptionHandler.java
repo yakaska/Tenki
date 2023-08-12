40db6a3788e757392bb51ec9e7a235d656b946c5
Valentin <valentin7petr@gmail.com>
2023-08-12 22:44:05 +0300

    Fix: empty exception handler

--- src/main/java/ru/yakaska/tenki/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yakaska/tenki/exception/GlobalExceptionHandler.java
@@ -1,12 +1,7 @@
 package ru.yakaska.tenki.exception;
 
-import org.springframework.http.*;
-import org.springframework.validation.*;
-import org.springframework.web.bind.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.*;
-
-import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
