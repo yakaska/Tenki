e9b06401d0f0e839eaede8160135b3bdc97ccb4b
Valentin <valentin7petr@gmail.com>
2023-08-12 22:43:52 +0300

    Fix: empty exception handler

--- src/main/java/ru/yakaska/tenki/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yakaska/tenki/exception/GlobalExceptionHandler.java
@@ -11,16 +11,5 @@ import java.util.*;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
 
 }
